7790c8df0aa9794c2009d65ad25b9ae37cf10e53
Rafael Thayto <rafa.thayto@gmail.com>
2018-04-09 21:58:54 -0300

    Creating routes

--- api/bilingCicle/billingCicleService.js
+++ api/bilingCicle/billingCicleService.js
@@ -5,4 +5,21 @@ const BillingCycle = require('./billingCicle')
 BillingCycle.methods(['get', 'post', 'put', 'delete'])
 BillingCycle.updateOptions({ new: true, runValidators: true })
 
+BillingCycle.route('count', (req, res, next) => {
+  BillingCycle.count((error, value) => {
+    if (error) {
+      res.status(500).json({ errors: [error] })
+    } else {
+      res.json({ value })
+    }
+  })
+})
+
+BillingCycle.route('summary', (req, res, next) => {
+  BillingCycle.aggregate({
+    $project: { credit: { $sum: '$credits.value', debt: { $sum: '$debts.value' } } },
+    $group: { _id: null, credit: { $sum: '$credit' }, debt: { $sum: '$dept' } }
+  })
+})
+
 module.exports = BillingCycle
